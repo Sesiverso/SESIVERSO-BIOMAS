f0124a846bf5a5fbb6ce38d7ba0b13b992255a1b
lg11111111 <94802700+lg11111111@users.noreply.github.com>
2024-08-19 07:48:45 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Adiciona um evento de clique ao botão com id "clickMe"
-document.getElementById('clickMe').addEventListener('click', function() {
-    // Exibe um alerta quando o botão é clicado
-    alert('Você clicou no botão!');
-});
